57f05385a382b04cfee2c3175c0f648bf77a41e3
Viraj Chitlangia <virajchitlangia15@gmail.com>
2026-03-11 10:08:42 +0530

    Feature (Uc2): Print HelloApp to console

--- HelloApp2.java
+++ HelloApp2.java
@@ -1,7 +1,12 @@
 public class HelloApp2 {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+        if(args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } 
+        else {
+            System.out.println("Hello User");
+        }
+
+    }
 }
