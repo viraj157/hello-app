89d079dc833f7e6f8edf08a10653a6065e6fb960
Viraj Chitlangia <virajchitlangia15@gmail.com>
2026-03-18 08:24:16 +0530

    Feature UC4: Display Hello with Command-Line Argument or Default Message

---
+++ HelloApp4.java
@@ -0,0 +1,26 @@
+public class HelloApp4 {
+
+    public static void main(String[] args) {
+
+        
+        String name = "World";
+
+        
+        if (args.length > 0) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
